82a0204c8716cbdbe007e96de1ceee1364dce2a2
Sirija Kasi <sirijagowd00@gmail.com>
2021-04-25 13:00:56 +0530

    adding files

---
+++ c.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ReactDom } from "reactdom";
+
+class New extends React.Component {
+  render() {
+    return;
+    {
+      <h1>Project</h1>;
+    }
+  }
+}
+
+ReactDom.render(<New />, document.getElementById(main));
